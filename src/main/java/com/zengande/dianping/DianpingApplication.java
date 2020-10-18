f9a4a0862cf664b3583006061db1f0a0db46f2fb
zengande <zengande@outlook.com>
2020-10-18 10:55:55 +0800

    修改配置

--- src/main/java/com/zengande/dianping/DianpingApplication.java
+++ src/main/java/com/zengande/dianping/DianpingApplication.java
@@ -1,9 +1,11 @@
 package com.zengande.dianping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zengande.dianping.dao")
 public class DianpingApplication {
 
     public static void main(String[] args) {
